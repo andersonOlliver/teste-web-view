ad4232d37cd87cc7ad4fd1e82788ed0af3362944
Anderson Olliver <pla.olliver@gmail.com>
2017-07-11 13:46:02 -0400

    Teste

---
+++ TesteWebView/TesteWebView/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TesteWebView.ViewModel;
+using Xamarin.Forms;
+
+namespace TesteWebView
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainPageViewModel();
+        }
+    }
+}
---
+++ TesteWebView/TesteWebView/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteWebView.ViewModel
+{
+    public class MainPageViewModel
+    {
+        public string Endereco { get; set; }
+
+        public MainPageViewModel()
+        {
+            Endereco = "https://www.google.com.br";
+        }
+    }
+}
